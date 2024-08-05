d63270e09bfc18852693aa62bab75daeef65e5ef
anis boubidi <boubidi.anisnjm@gmail.com>
2024-08-05 20:04:04 +0100

    02/08
    first day

---
+++ src/app/lib/prisma.ts
@@ -0,0 +1,9 @@
+import { clients } from "@clerk/clerk-sdk-node";
+import { Client } from "@clerk/nextjs/server";
+import { PrismaClient } from "@prisma/client";
+import { count } from "console";
+declare global{
+    var prisma: PrismaClient | undefined 
+}
+export const client = globalThis.prisma || new PrismaClient()
+if (process.env.NODE_ENV !== "production") globalThis.prisma= clients
