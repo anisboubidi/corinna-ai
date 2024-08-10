11bd7f85c2590ece5dcc610d1e1f9df5913e98a8
anis boubidi <boubidi.anisnjm@gmail.com>
2024-08-10 16:07:05 +0100

    35

---
+++ src/context/use-auth-context.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+type InitialValuesProps = {
+    currentStep : number
+    setCurrentStep : React.Dispatch<React
+}
+
+const use-auth-context = (props: Props) => {
+  return (
+    <div>use-auth-context</div>
+  )
+}
+
+export default use-auth-context
